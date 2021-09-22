17fad80850ab57dd2ee3d6f30789d880ad830b46
= <aspironsv@gmail.com>
2021-09-22 23:45:46 +0500

    added notification service

---
+++ services/SMSGatewayService.js
@@ -0,0 +1,109 @@
+class Eskiz{
+    token = ''
+	constructor(email = '', secret = '') {
+		
+		if(email != '' && secret != ''){
+			smsdata = {
+				'email' : email,
+				'password' : secret
+            }
+
+		}else{
+			smsdata = {
+				'email' : SMS_GATEWAY_EMAIL,
+				'password' : SMS_GATEWAY_SECRET
+            }
+		}
+
+		if(SMS_GATEWAY_TOKEN == ''){
+			sms_gateway_data = JSON.parse(this.sendPing(smsdata, 'auth/login', 'oauth'), 1)
+			if(sms_gateway_data['message'] == 'token_generated'){
+				file_put_contents(SMS_GATEWAY_TOKEN_FILE, sms_gateway_data['data']['token'])
+			}
+			token = sms_gateway_data['data']['token']
+		}else{
+			
+			token = file_get_contents(SMS_GATEWAY_TOKEN_FILE, sms_gateway_data['data']['token'])
+			
+			data = this.sendPing([], 'auth/user')
+			// print_r(data)
+			if(data['message'] != 'authenticated_user' && data['data']['status'] !== 'active'){
+				data = this.sendPing([], 'auth/refresh', 'PATCH')
+				
+			}
+		}
+
+		this.token = token
+	}
+	// getToken - get token from Eskiz.uz GateWay
+	getToken(data) { 
+		output = this.sendPing(data, 'auth/login', 'oauth')  // type is oauth when token is needed
+		// print_r(output)
+		if(output['message'] === 'token_generated'){
+			output = output['data']['token']
+		}
+		return output
+	}
+	
+	// sendSMS - send SMS to a number through Eskiz.uz GateWay
+	sendSMS(data) { 
+
+		output = this.sendPing(data, 'message/sms/send')
+		// print_r(output)
+		output['is_sent'] = 'no'
+		if(output['status'] == 'waiting'){
+			output['is_sent'] = 'yes'			
+		}
+		
+		return output
+	}
+	
+	// getSMS - get requests from Eskiz.uz GateWay
+	getSMS(sms_id ) { 
+
+		output = this.sendPing([], 'message/sms/get/status/' . sms_id)
+		// print_r(output)
+		if(output['status'] === 'success' && output['message']['status'] === 'Delivered'  ){
+			output = output['message']['status']
+		}
+		return output
+	}
+	
+	// sendPing - sending/getting(SMS)/refresh_token/get_token using Eskiz.uz GateWay
+	sendPing(content, method, type = '') { // type is oauth when token is needed
+
+		
+			curl = curl_init() 
+			// set url 
+			curl_setopt(curl, CURLOPT_URL, SMS_GATEWAY_URL.method) 
+			curl_setopt(curl, CURLOPT_RETURNTRANSFER, 1) 
+            
+            // when you need to update token
+			if(type == 'PATCH'){
+				curl_setopt(curl, CURLOPT_CUSTOMREQUEST, 'PATCH')
+			}
+
+			
+			// by default oauth token is set up in every request's header
+			if(type !== 'oauth'){
+				headers = array(
+					sprintf('Authorization: Bearer %s', this.token)
+				)
+				curl_setopt(curl, CURLOPT_HTTPHEADER, headers)
+			}
+            
+            // when you need to send a message
+			if(!empty(content)){
+				curl_setopt(curl, CURLOPT_POST, 1)
+			
+				curl_setopt(curl, CURLOPT_POSTFIELDS, content)
+			}
+			
+			// output contains the output string 
+			output = curl_exec(curl) 
+
+			curl_close(curl)      
+		// file_put_contents("return_sent.txt", output)
+		return json_decode(output, true)
+	}
+}
